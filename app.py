474f5cd85abf60e579b6dc83f9a4ce9f89b22429
Omar Salah <2401601@student.eelu.edu.eg>
2026-08-22 03:41:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 1. إعدادات الصفحة (Apple & Notion Minimalist Style)
-st.set_page_config(page_title="Omar's Data Roadmap", page_icon="🧭")
+st.set_page_config(page_title="DataLab | by Omar Saleh", page_icon="🧭")
 
 # 2. بعض اللمسات الجمالية للتنسيق
 st.markdown("""
@@ -13,7 +13,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # 3. هيكل البيانات واللينكات الحقيقية
-chapters = {
+Phase = {
     "01": {
         "title": "Understanding Data", 
         "link": "https://www.linkedin.com/posts/omarsaleh-cs_%D9%82%D8%B5%D8%A9-%D9%83%D8%B1%D8%B3%D9%8A-%D9%82%D8%AF%D9%8A%D9%85-%D8%A3%D8%B8%D9%87%D8%B1%D8%AA-%D8%B3%D8%B1-%D8%A3%D8%BA%D9%84%D9%89-%D9%85%D8%AC%D8%A7%D9%84-%D9%81%D9%8A-%D8%A7%D9%84%D8%B9%D8%A7%D9%84%D9%85-activity-7491739030548664320-wMk4?utm_source=share&utm_medium=member_android&rcm=ACoAAFfGkf4Bchnn1vdJPOkg2UryBOqvABwrOGk"
@@ -32,15 +32,15 @@ chapters = {
 }
 
 # 4. تصميم الواجهة الرئيسية
-st.markdown("<h1 class='title'> Data Analytics Roadmap</h1>", unsafe_allow_html=True)
-st.markdown("<p class='subheader'>رحلتي في مجال عالم البيانات من الصفر حتى الاحتراف</p>", unsafe_allow_html=True)
+st.markdown("<h1 class='title'> DataLab | by Omar Saleh</h1>", unsafe_allow_html=True)
+st.markdown("<p class='subheader'>مرجعك الشامل وخريطتك العملية من تحليل البيانات إلى علوم البيانات والذكاء الاصطناعي</p>", unsafe_allow_html=True)
 st.divider()
 
 # عرض الأزرار بشكل تفاعلي ومرتب
-for ch, info in chapters.items():
+for ph, info in Phase.items():
     col1, col2 = st.columns([1, 4])
     with col1:
-        st.button(f"Ch {ch}", disabled=True)
+        st.button(f"ph {ph}", disabled=True)
     with col2:
         if info["link"]:
             st.link_button(f"Read: {info['title']}", url=info["link"])
@@ -48,4 +48,4 @@ for ch, info in chapters.items():
             st.button(f"{info['title']} - قريباً ⏳", disabled=True)
 
 st.divider()
-
+st.caption(Engineered & Maintained by Omar Saleh)
